bb49de9d02bf412e66e986ee8c1d3697fa366491
Jean-Mathieu Emond <jean-mathieu.emond@jmdev.ca>
2020-06-02 15:58:25 -0400

    Fixing UpdateRequest for site setting

--- app/Http/Requests/SiteSetting/UpdateRequest.php
+++ app/Http/Requests/SiteSetting/UpdateRequest.php
@@ -24,7 +24,7 @@ class UpdateRequest extends FormRequest
      */
     public function rules()
     {
-        $id = $this->route('tag');
+        $id = $this->route('setting');
 
         return [
             'key' => [
